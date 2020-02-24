2dd74976d65fc43c8eaf4c444b9894105aa739c0
Ramesh Naidu <reengineer1999@gmail.com>
2020-02-24 17:14:36 +0530

    app.js file

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port port!');
+});
+
+//Run app, then load http://localhost:port in a browser to see the output.
